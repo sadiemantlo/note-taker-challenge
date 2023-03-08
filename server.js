beaa30617db42ffe0826f2461a7cd8edb50f2cc0
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-03-07 16:13:48 -0800

    set up notes html path

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 // require
 const express = require('express');
+const path = require('path');
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 // initialization 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 app.use(express.json());
 app.use(express.urlencoded( {extended: true} ));
 app.use(express.static('public'));
 // app.use('./api', apiRoutes);
 // app.use('/', htmlRoutes);
-
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
 // open server PORT
 app.listen(PORT, () => console.log(`listening on PORT ${PORT} :)`));
