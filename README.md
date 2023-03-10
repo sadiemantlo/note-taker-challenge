ba2f9304fc9975fa23db577048943c1e08802848
Sadie Mantlo <sadiemantlo@Sadies-MacBook-Air.local>
2023-03-09 20:54:58 -0800

    added id's to notes and a readme file

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 ## **Description**
 
-This is a note taking application. User can adds and save notes to a 
+This is a note taking application. User can add and save notes to a 
 ## **Installation**
 
+Run "npm i" in the terminal to install all the dependencies required. 
+
 ## **Usage**
+
+To use this application, users simply add a note title and note text in the text boxes. To save a note, click the save icon on the top right of the page. You can reopen a nore by simply clikcing on the note title on the left side of the page.
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,6 +3,7 @@ const path = require('path');
 const db = require('../db/db.json');
 const fs = require('fs');
 
+
 // gets the saved notes array 
 router.get('/notes', (req, res) => {
     const savedNotes = db;
@@ -13,12 +14,40 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
     const savedNotes = db;
     const newNote = req.body; 
+    req.body.id = getID();
     savedNotes.push(newNote);
     console.log(savedNotes);
     fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(savedNotes));
     res.status(200).json(savedNotes);
 });
 
+// adds an id to each note (check for a better way bc this is alot)
+let notesArray = [];
+fs.readFile('./db/db.json', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    const parsedNotes = JSON.parse(data);
+    for (let i = 0; i < parsedNotes.length; i++) {
+        notesArray.push(parsedNotes[i]);
+    }
+});
 
+const getID = () => {
+    let noteID = Math.floor(Math.random() * 99) + 1;
+    if (notesArray.length === 0) {
+        notesArray.push(noteID);
+    } else {
+        for (let i = 0; i < notesArray.length; i++) {
+            if (notesArray[i] === noteID) {
+                noteID = Math.floor(Math.random() * 99) + 1;
+                i = 0;
+            }
+        }
+        notesArray.push(noteID);
+    }
+    return notesArray[notesArray.length - 1]; 
+}
 
 module.exports = router;
